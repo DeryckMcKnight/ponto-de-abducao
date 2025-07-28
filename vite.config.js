8751b45add26ad04f3a50e7cca4080c775ae27a9
DeryckMcKnight <derycklima@gmail.com>
2025-07-28 10:10:56 -0400

    Corrigindo workflow do GitHub Actions: Adicionando passos de instalação e build

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import path from "path"
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/ponto-de-abducao/',
+  base: './',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
